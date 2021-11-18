9a233fd01556fede2cdc307edca5d8e6b3dbf8dc
Harry Chen <harrychen0314@gmail.com>
2021-11-17 22:02:43 -0800

    draft about page

---
+++ components/team/section.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="py-4 py-lg-8">
+    <v-row>
+      <v-col cols="12" md="3">
+        <div style="width: 80px; height: 4px" class="mb-3 secondary" />
+        <h2 class="text-h4 mb-3">
+          {{ header }}
+        </h2>
+      </v-col>
+      <v-col cols="12" md="8" class="pa-md-0">
+        <div v-for="(item, i) in team" :key="i">
+          <v-divider v-if="i !== 0" />
+          <div class="d-flex align-center py-3">
+            <div class="mr-4">
+              <v-avatar size="190">
+                <v-img :src="item.image" />
+              </v-avatar>
+            </div>
+            <div>
+              <div class="text-h5">
+                {{ item.name }}
+              </div>
+              <div class="text-h6 secondary--text">
+                {{ item.title }}
+              </div>
+              <div class="text-body mt-3">
+                {{ item.bio }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    team: {
+      type: Array,
+      default: () => []
+    },
+    header: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-sheet color="surface">
+      <v-container class="py-6 py-lg-12 text-center">
+        <h1 class="text-h4 text-sm-h3 text-md-h2 text-lg-h1">
+          All about <span class="primary--text">TeamsCode</span>
+        </h1>
+        <h2 class="text-h6 text-sm-h5 text--lighten-2 mt-4 ">
+          Our history, mission, and team
+        </h2>
+      </v-container>
+    </v-sheet>
+    <v-sheet>
+      <v-row>
+        <v-col class="pa-0 d-none d-md-block" md="4">
+          <v-img
+            src="/images/about-background.webp"
+            height="100%"
+            alt=""
+          />
+        </v-col>
+        <v-col class="align-center">
+          <div class="pa-6 pa-md-12">
+            <v-responsive max-width="900">
+              <div class="text-uppercase font-weight-bold body-2 primary--text mb-2">
+                TeamsCode
+              </div>
+              <h2 class="text-h3 text-lg-h2">
+                From in-person events to virtual contests
+              </h2>
+              <div class="text-body mt-5">
+                <div>
+                  TeamsCode is a student-run 501(c)(3) non-profit that works to encourage middle school and high school students to study computer science. We work with teachers and sponsors to create awesome programming contests – both online and in-person!
+                </div>
+                <br>
+                <div>
+                  Chris Elliott and Alan Bi founded TeamsCode in the beginning of 2017. Both of them participated in local programming contests and noticed a lack of representation from their school. They set about to change that by organizing the first ever Mercer Island High School Programming Contest on April 1st, 2017. Since then, TeamsCode has expanded its influence across three states in in-person events.
+                </div>
+                <br>
+                <div>
+                  Under the impact of a global pandemic, TeamsCode launched its first virtual programming contest in summer 2020. With more than 290 participants showing up to the event, it became TeamsCode's largest contest at that time. Since our first virtual contest, TeamsCode's influence expanded from the United States to more then 20 countries as we organized several more virtual events, showing a huge improvement in problem quality, contest experience and ultimately contest participation.
+                </div>
+                <br>
+                <div>
+                  As of summer 2021, TeamsCode's contest has 3 divisions featuring curated problems in different difficulties. In the future, we hope to continue our mission of bringing an enjoyable and inspirational team programming contest to everyone interested in computer science.
+                </div>
+              </div>
+            </v-responsive>
+          </div>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <v-sheet color="transparent">
+      <v-container class="mt-8 py-4 py-lg-8">
+        <div class="text-center">
+          <div class="text-uppercase font-weight-bold body-2 primary--text mb-2">
+            Organizers
+          </div>
+          <h2 class="text-h3 text-lg-h2">
+            Meet the team
+          </h2>
+        </div>
+      </v-container>
+    </v-sheet>
+    <v-sheet color="transparent">
+      <div class="py-4">
+        <TeamSection header="Executives" :team="team" />
+      </div>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      team: [{
+        name: 'Ethen Peng',
+        title: 'Co-President',
+        image: '/images/teams/ethan.webp',
+        bio: 'Ethan is a senior at Los Altos High School. He started learning programming two years ago, and competitive programming one year ago. He is excited to help bring quality contests to computer science students. Outside of programming, he plays tennis and watches Chinese dramas.'
+      }, {
+        name: 'Aayush Sheth',
+        title: 'Co-President',
+        image: '/images/teams/aayush.webp',
+        bio: 'Aayush is a senior at Tesla STEM Highschool in Washington state. He started learning competitive programming four years ago and is well-versed in C++ and Java. Outside of school, he enjoys learning economics, piano, and soccer. Through TeamsCode he hopes to develop his leadership and coordination skills.'
+      }, {
+        name: 'Joe Li',
+        title: 'Vice President - Logistics',
+        image: '/images/teams/joe.webp',
+        bio: 'Joe Li is a sophomore at The Harker School. He is fluent in C++ and Java, and he is a current USACO Platinum Competitor. In addition to competitive programming, he also does AI and web development. He also enjoys violin and karate. As a Teamscode officer, he is happy to contribute to an organization that promotes competitive programming.'
+      }, {
+        name: 'Elijah Huang',
+        title: 'Vice President - Problem Writing',
+        image: '/images/teams/elijah.webp',
+        bio: 'Elijah Huang is a senior at Basis Independent Silicon Valley in California. He started programming a year ago and is currently a USACO Platinum Competitor who has scored 700+ on the US Open and a Master on Codeforces. Apart from programming, Elijah enjoys binging a ludicrous amount of anime and going all out while rock climbing. He is thrilled to combine his passion and algorithmic knowledge to show others the beauty of programming.'
+      }, {
+        name: 'Harry Chen',
+        title: 'Vice President - Technology',
+        image: '/images/teams/harry.webp',
+        bio: 'Harry is a sophomore at Northwood High School in Irvine, California. He started competitive programming in C++ 2 years ago and is currently in USACO Gold. In addition to that, Harry also does full-stack web development. He maintains TeamsCode\'s website and contest system. Harry is excited to bring enjoyable contests to TeamsCoders. In his free time, he likes to play tennis and watch movies.'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
