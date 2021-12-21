314cd380913f8081da775df72ed37f4d9d307a3e
Harry Chen <harrychen0314@gmail.com>
2021-12-20 21:00:42 -0800

    migrate to sendinblue

---
+++ components/newsletter/form.vue
@@ -0,0 +1,16 @@
+
+<!-- END - We recommend to place the below code where you want the form in your website html  -->
+<!-- End Sendinblue Form -->
+<template>
+  <div>
+    <v-form
+      id="sib-form"
+      method="POST"
+      action="https://99250bc0.sibforms.com/serve/MUIEADzF0ac0VSwx21liDrHUZI3PzssByomPQW4UhYD-VGWbzgDoEJspU1O7ircq6wN8vOdctMORia4qKbhrpaRxcwaHL8O5kMl1VX2yYDoGIRyJNMhRSh-IfeWF7BcnyKlW28YrzLc7Zm0tdEQt5pRpTT2tsOPh89kylUlDcbt6nF0lcY0M2Kg2Q_YJ2Z-xrobjwhset7xR52Cz"
+      novalidate
+      target="_blank"
+    >
+      <slot />
+    </v-form>
+  </div>
+</template>
